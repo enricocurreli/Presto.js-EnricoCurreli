e9378da6e4ab8cf841f5b8ea14e4aec26cc39872
Enrico <enricocurreli.ec@icloud.com>
2024-03-21 00:04:47 +0100

    primo commit, qualche bag da risolvere e inserire nuove funzionalità

---
+++ README.md
@@ -0,0 +1,51 @@
+# IMPOSTAZIONE PROGETTO FRONT-END
+
+*EVENTO SCROLL*
+
+
+let navbar =  document.querySelector(".navbar")
+window.addEventListener("scroll", ()=>{
+
+    if(window.scrollY>200){
+        navbar.style.backgroundColor =  blue
+
+    } else {
+
+    }
+})
+
+<!-- LIBRERIA AOS -->
+
+
+
+CSS:
+
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+JS:
+
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script>
+  AOS.init();
+</script>
+
+
+<!-- CHIAMATE ASINCRONE -->
+
+setInterval -> ci permette di eseguire un blocco di codice ogni tot numero di millisecondi
+
+setInterval( ()=>{
+
+
+}, 3000);
+
+clearIntervall -> ferma il setInterval
+
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,82 @@
+let navbar = document.querySelector(".navbar")
+let navbarBrand = document.querySelector("#navbarBrand")
+
+
+
+
+window.addEventListener("scroll", () => {
+    
+    if(window.scrollY > 240){
+        
+        navbar.classList.add("nav-scrolled")
+        navbarBrand.classList.add("transition")
+        navbarBrand.innerHTML= "";
+        navbarBrand.innerHTML= `<img src="media/sardinia.png">`
+        
+        
+        
+        
+    } else {
+        navbar.classList.remove("nav-scrolled")
+        navbarBrand.classList.add("transition")
+        navbarBrand.innerHTML= `
+        <figcaption class="resort-text display-2"> I Corsari - Resort</figcaption>`
+        
+        
+    }
+    
+})
+
+
+// let logoNav = documents.query.querySelector("#logoNav")
+
+// window.addEventListener("scroll", () => {
+
+//     if(window.scrollY > 240){
+
+//         logoNav.classList.add("d-none")
+
+//     } else {
+//         logoNav.classList.remove("d-none")
+//     }
+
+// })
+
+
+let numBook = document.querySelector("#numBook")
+let numFam = document.querySelector("#numFam")
+let numCouple = document.querySelector("#numCouple")
+
+
+function createInterval(elementId, finalNumber, frequency){
+    let counter = 0
+    
+    let intervallo = setInterval(() => {
+        if(counter < finalNumber){
+            counter++
+            elementId.innerHTML = counter;
+        } else {
+            clearInterval(intervallo)
+        }
+    }, frequency);
+}
+
+// createInterval(numBook, 80, 60)
+// createInterval(numFam, 50, 105)
+// createInterval(numCouple, 30, 180)
+
+
+let cntNum = document.querySelector("#cntNum")
+
+window.addEventListener("scroll", () => {
+    
+    if(window.scrollY > 150){
+        
+       
+        
+        
+    } else {
+        
+    }
+    
+})
